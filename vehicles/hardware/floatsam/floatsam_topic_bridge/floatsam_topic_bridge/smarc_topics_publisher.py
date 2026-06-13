176233800d4fd6c57188436f33f95425b108a36b
doclorenzo <mannolorenzo421@gmail.com>
2026-06-13 17:54:10 +0000

    ASKO coordinate hard-coded

--- vehicles/hardware/floatsam/floatsam_topic_bridge/floatsam_topic_bridge/smarc_topics_publisher.py
+++ vehicles/hardware/floatsam/floatsam_topic_bridge/floatsam_topic_bridge/smarc_topics_publisher.py
@@ -29,8 +29,8 @@ from smarc_utilities.georef_utils import convert_latlon_to_utm
 from floatsam_controllers.floatsam_common import FloatSam
 from floatsam_topic_bridge.floatsam_tf_helpers import FloatSamTransforms
 
-ASKO_LAT = 59.3070981
-ASKO_LON = 18.7085827
+ASKO_LAT = 58.8233198
+ASKO_LON = 17.634154
 
 class SmarcTopicsPublisher(Node):
     """
